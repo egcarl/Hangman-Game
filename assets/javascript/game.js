1d76df36737ade37c18007cde18af04ebc20fd30
Eric Carl <egcarl@gmail.com>
2018-03-15 18:19:06 -0700

    udpate counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,6 +20,12 @@ var remainingLetters = computerWord.length;
 
 //The game
 
+document.onkeyup = function (event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    gameUpdate (letterGuessed);
+    checkWin ();
+}
+
 function startGame () {
     // pick random word, create guesses left, create blank answer bank
     computerWord = wordsLibrary[Math.floor(Math.random() * wordsLibrary.length)];
@@ -34,21 +40,21 @@ function startGame () {
     }
 }
 
-function gameUpdate (letter) {
+function gameUpdate (letterGuessed) {
     //for each letter entered, decrease guesses left by 1, udpate page
     guessesLeft--;
     document.getElementById('guessesLeft').innerHTML = guessesLeft;
 
     //if the letter doesn't match the computer word, push the letter to the wrong guesses
-    if (computerWord.indexOf(letter) === -1) {
-        wrongGuesses.push(letter);
+    if (computerWord.indexOf(letterGuessed) === -1) {
+        wrongGuesses.push(letterGuessed);
         wrongGuesses.innerHTML = wrongGuesses;
     }
     else  {
         // if letter in computer word matches letter entered, show letter in 
         for (var i = 0; i < computerWord.length; i++) {
-            if (computerWord[i] === letter) {
-            computerWord[i] = letter;
+            if (computerWord[i] === letterGuessed) {
+            computerWord[i] = letterGuessed;
             }
         }
     }
@@ -57,29 +63,29 @@ function gameUpdate (letter) {
 
 function checkWin () {
     //check if no letters left of mystery word
-    if (computerWord.indexOf('_')=== -1) {
+    if (answers.indexOf('_')=== -1) {
         alert('Win!');
+        win++;
+        document.getElementById('wins').innerHTML = wins;
     }
     // if guesses run out, then lose
     else if (guessesLeft === 0) {
         alert ('You\'ve lost!')
+        losses++;
+        document.getElementById('losses').innerHTML = losses;
     }
 }
     
-document.onkeyup = function (event) {
-    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-    gameUpdate (letterGuessed);
-    checkWin ();
-}
+
 
 
 
 startGame();
 
 
-document.getElementById('wins').innerHTML = wins;
+
 document.getElementById('computerWord').innerHTML = answers;
-document.getElementById('losses').innerHTML = losses;
+
 document.getElementById('guessesLeft').innerHTML = guessesLeft;
 
 document.getElementById('wrongGuesses').innerHTML = wrongGuesses;
